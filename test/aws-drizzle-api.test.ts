c74333d72407122add7e5a65214ebc0dfc3dbe0c
Johannes Mueller <johannes.mueller@avl.com>
2024-05-07 14:44:14 +0200

    Initial commit

---
+++ test/aws-drizzle-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsDrizzleApi from '../lib/aws-drizzle-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-drizzle-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsDrizzleApi.AwsDrizzleApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
